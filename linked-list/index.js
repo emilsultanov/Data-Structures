78770719e25d04e70a4b590dcdd6fd19580a209a
Emil Sultanov <emil.sultanov@cybernet.az>
2022-06-21 23:35:58 +0400

    Implement doubly-linked-list

--- linked-list/index.js
+++ linked-list/index.js
@@ -1,16 +1,21 @@
-class SinglyLinkedListNode{
-  constructor(data){
-    this.data = data;
-    this.next = null
-  }
+/**
+ *
+ * Singly-Linked-List
+ *
+ */
+class SinglyLinkedListNode {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
 }
 
-class SinglyLinkedList{
-  constructor(head = null){
-    this.head = head;
-  }
+class SinglyLinkedList {
+	constructor(head = null) {
+		this.head = head;
+	}
 
-  	size() {
+	size() {
 		let size = 0;
 		let node = this.head;
 
@@ -28,3 +33,40 @@ let singlyLinkedListNode2 = new SinglyLinkedListNode(6);
 singlyLinkedListNode1.next = singlyLinkedListNode2;
 
 let SinglyLinkedList1 = new SinglyLinkedList(singlyLinkedListNode1);
+
+/**
+ *
+ * Doubly-Linked-List
+ *
+ */
+
+class DoublyLinkedListNode {
+	constructor(data) {
+		this.data = data;
+		this.prev = null;
+		this.next = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor(head = null) {
+		this.head = head;
+	}
+}
+
+let doublyLinkedListNode1 = new DoublyLinkedListNode(3);
+let doublyLinkedListNode2 = new DoublyLinkedListNode(5);
+let doublyLinkedListNode3 = new DoublyLinkedListNode(7);
+let doublyLinkedListNode4 = new DoublyLinkedListNode(9);
+
+doublyLinkedListNode1.next = doublyLinkedListNode2;
+
+doublyLinkedListNode2.prev = doublyLinkedListNode1;
+doublyLinkedListNode2.next = doublyLinkedListNode3;
+
+doublyLinkedListNode3.prev = doublyLinkedListNode2;
+doublyLinkedListNode3.next = doublyLinkedListNode4;
+
+doublyLinkedListNode4.prev = doublyLinkedListNode3;
+
+let doublyLinkedList1 = new DoublyLinkedList(doublyLinkedListNode1);
